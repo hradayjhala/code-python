0971368669407348424ce7ba5797527c62cef7fd
hradayjhala <75161221+hradayjhala@users.noreply.github.com>
2020-11-28 18:23:56 +0800

    Create value_checker.py

---
+++ basic/value_checker.py
@@ -0,0 +1,9 @@
+x = int(input("what is the value of x?"))
+y = int(input("what is the value of y?"))
+
+if (x < y):
+    st = "x is less than y"
+else:
+    st = "x is greater than y"
+    
+print(st)
