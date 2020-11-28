6cd74b3b80e48ac324c851baf13d1e213417c2bd
hradayjhala <75161221+hradayjhala@users.noreply.github.com>
2020-11-28 18:14:33 +0800

    Delete value_checker.py

--- value_checker.py
+++
@@ -1,9 +0,0 @@
-x = int(input("what is the value of x?"))
-y = int(input("what is the value of y?"))
-
-if (x < y):
-    st = "x is less than y"
-else:
-    st = "x is greater than y"
-    
-print(st)
